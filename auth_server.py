5fd1ecbb2d76e612304bf2b1d4a217800f92ac18
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-10-02 20:49:43 -0400

    Switch to fast api

--- auth_server.py
+++ auth_server.py
@@ -1,100 +1,98 @@
-from flask import Flask, jsonify
-import utils.db_interface as database
-import utils.password_hasher as hasher
-from flask import send_file
-from flask_cors import CORS, cross_origin
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 import os
-from flask import Flask, send_from_directory
-import utils.email_checker as email_interface
+from utils import db_interface as database
+from utils import password_hasher as hasher
+from utils import email_checker as email_interface
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
 
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.route('/')
-def home():
-    return 'Welcome to Lif Auth Server!'
+@app.get("/")
+async def home():
+    return "Welcome to Lif Auth Server!"
 
-@app.route('/.well-known/acme-challenge/<filename>')
-def acme_challenge(filename):
-    return send_from_directory('.well-known/acme-challenge', filename)
+@app.get("/.well-known/acme-challenge/{filename}")
+async def acme_challenge(filename):
+    return FileResponse(f".well-known/acme-challenge/{filename}")
 
-@app.route('/login/<username>/<password>')
-@cross_origin()
-def login(username, password):
+@app.get("/login/{username}/{password}")
+async def login(username: str, password: str):
     # Gets password hash
     password_hash = hasher.get_hash_with_database_salt(username=username, password=password)
 
-    # Checks if password hash was successful 
-    if password_hash == False:
-        return jsonify({"Status": "Unsuccessful", "Token": "None"})
-    
+    # Checks if password hash was successful
+    if not password_hash:
+        return {"Status": "Unsuccessful", "Token": "None"}
+
+    # Verifies credentials with database
+    status = database.verify_credentials(username=username, password=password_hash)
+
+    if status == "Good!":
+        # Gets token from database
+        token = database.retrieve_user_token(username=username)
+
+        # Returns info to client
+        return {"Status": "Successful", "Token": token}
     else:
-        # Verifies credentials with database
-        status = database.verify_credentials(username=username, password=password_hash)
+        # Tells client credentials are incorrect
+        return {"Status": "Unsuccessful", "Token": "None"}
 
-        if status == "Good!":
-            # Gets token from database
-            token = database.retrieve_user_token(username=username)
-
-            # Returns info to client
-            return jsonify({"Status": "Successful", "Token": token})
-        
-        else: 
-            # Tells client credentials are incorrect
-            return jsonify({"Status": "Unsuccessful", "Token": "None"})
-        
-@app.route('/verify_token/<username>/<token>')
-def verify_token(username, token): 
-    # Gets token from database 
+@app.get("/verify_token/{username}/{token}")
+async def verify_token(username: str, token: str):
+    # Gets token from database
     database_token = database.retrieve_user_token(username=username)
 
-    if database_token == False: 
-        return jsonify({"Status": "Unsuccessful"})
-    
+    if not database_token:
+        return {"Status": "Unsuccessful"}
     elif database_token == token:
-        return jsonify({"Status": "Successful"})
-    
-    else: 
-        return jsonify({"Status": "Unsuccessful"})
-    
-@app.route('/get_pfp/<username>')
-def get_pfp(username):
-    # Checks if the use has a profile pic uploaded
-    if os.path.isfile(f'user_images/pfp/{username}'):
-         return send_file(f'user_images/pfp/{username}', mimetype='image/gif')
-    
-    else: 
+        return {"Status": "Successful"}
+    else:
+        return {"Status": "Unsuccessful"}
+
+@app.get("/get_pfp/{username}")
+async def get_pfp(username: str):
+    # Checks if the user has a profile pic uploaded
+    if os.path.isfile(f"user_images/pfp/{username}"):
+        return FileResponse(f"user_images/pfp/{username}", media_type='image/gif')
+    else:
         # Returns default image if none is uploaded
-         return send_file('user_images/pfp/default.png', mimetype='image/gif')
-    
-@app.route('/get_banner/<username>')
-def get_banner(username):
-    # Checks if the use has a profile pic uploaded
-    if os.path.isfile(f'user_images/banner/{username}'):
-         return send_file(f'user_images/banner/{username}', mimetype='image/gif')
-    
-    else: 
+        return FileResponse('user_images/pfp/default.png', media_type='image/gif')
+
+@app.get("/get_banner/{username}")
+async def get_banner(username: str):
+    # Checks if the user has a profile pic uploaded
+    if os.path.isfile(f"user_images/banner/{username}"):
+        return FileResponse(f"user_images/banner/{username}", media_type='image/gif')
+    else:
         # Returns default image if none is uploaded
-         return send_file('user_images/banner/default.png', mimetype='image/gif')
-    
-@app.route('/create_account/<username>/<email>/<password>')
-def create_account(username, email, password):
+        return FileResponse('user_images/banner/default.png', media_type='image/gif')
+
+@app.get("/create_account/{username}/{email}/{password}")
+async def create_account(username: str, email: str, password: str):
     # Check username usage
     username_status = database.check_username(username)
-    if username_status == True:
+    if username_status:
         return {"status": "unsuccessful", "reason": "Username Already in Use!"}
-    
+
     # Check email usage
     email_status = database.check_email(email)
-    if email_status ==  True:
+    if email_status:
         return {"status": "unsuccessful", "reason": "Email Already in Use!"}
-    
+
     # Check if email is valid
     email_isValid = email_interface.is_valid_email(email)
-    if email_isValid == False:
+    if not email_isValid:
         return {"status": "unsuccessful", "reason": "Email is Invalid!"}
-    
+
     # Hash user password
     password_hash = hasher.get_hash_gen_salt(password)
 
@@ -104,4 +102,5 @@ def create_account(username, email, password):
     return {"status": "ok"}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8002)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
